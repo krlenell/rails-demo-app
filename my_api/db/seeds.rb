240128a3e7cfdc703813f8024291fbd6eb67c48d
Kevin Lenell <krlenell@gmail.com>
2020-12-10 20:13:46 -0800

    created api using howtojsonapi tutorial

--- my_api/db/seeds.rb
+++ my_api/db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+sushi_place = Restaurant.create!(name: 'Sushi Place', address: '123 Main Street')
+burger_place = Restaurant.create!(name: 'Burger Place', address: '456 Other Street')
+
+sushi_place.dishes.create!(name: 'Volcano Roll', rating: 3)
+sushi_place.dishes.create!(name: 'Salmon Nigiri', rating: 4)
+
+burger_place.dishes.create!(name: 'Barbecue Burger', rating: 5)
+burger_place.dishes.create!(name: 'Slider', rating: 3)
