a863e6d05ab2017e8c8dfed2f02dca3e124629f8
Kevin Lenell <krlenell@gmail.com>
2020-12-12 20:04:53 -0800

    hiatus to do models stuff

--- aa_demo/app/controllers/users_controller.rb
+++ aa_demo/app/controllers/users_controller.rb
@@ -2,4 +2,12 @@ class UsersController < ApplicationController
   def index
     render plain: "index action baybee"
   end
+
+  def show
+    render json: params
+  end
+
+  def create
+    render json: params
+  end
 end
